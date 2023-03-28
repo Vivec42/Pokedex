1756c7d8e2d550565fa36f67fcca296c69068d06
Quentin Silmain <quentin1813@yahoo.fr>
2023-03-28 10:54:55 +0200

    Added an array and tweaked it to work with a condition

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 
-function PokemonCard() {
-  return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
-}
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
+
+function PokemonCard() {
+  return (
+    <>
+      {pokemonList.map((pokemon) => (
+        <figure key={pokemon.name}>
+          {pokemon.imgSrc ? (
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+            <p>???</p>
+          )}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      ))}
+    </>
+  );
+}
