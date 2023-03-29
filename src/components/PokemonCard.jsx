94be8a23754bd9f7beb54254215043e5087ac08d
Quentin Silmain <quentin1813@yahoo.fr>
2023-03-29 08:51:25 +0200

    Added pokemonList to App.jsx and made a props to PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,31 +1,23 @@
 import React from "react";
 
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-export default PokemonCard;
-
-function PokemonCard() {
-  return (
-    <>
-      {pokemonList.map((pokemon) => (
-        <figure key={pokemon.name}>
-          {pokemon.imgSrc ? (
-            <img src={pokemon.imgSrc} alt={pokemon.name} />
-          ) : (
-            <p>???</p>
-          )}
-          <figcaption>{pokemon.name}</figcaption>
-        </figure>
-      ))}
-    </>
+function PokemonCard(props) {
+  return React.createElement(
+    React.Fragment,
+    null,
+    props.pokemonList.map(function (pokemon) {
+      return React.createElement(
+        "figure",
+        { key: pokemon.name },
+        pokemon.imgSrc
+          ? React.createElement("img", {
+              src: pokemon.imgSrc,
+              alt: pokemon.name,
+            })
+          : React.createElement("p", null, "???"),
+        React.createElement("figcaption", null, pokemon.name)
+      );
+    })
   );
 }
+
+export default PokemonCard;
